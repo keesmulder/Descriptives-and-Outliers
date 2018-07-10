298d8f476712ceaa208f001210d63699c64e03ec
milicamiocevic <40995653+milicamiocevic@users.noreply.github.com>
2018-07-10 15:22:39 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,8 +1,8 @@
 # Copyright statement:
 # This shiny apllication is developed by Milica Miocevic to be used for educational purposes.
-# Is is part of a program sponsered by the Education Incentive Funds of Utrecht University. 
-# The lay-out for the shiny applications for this program is developed by Kimberley Lek. 
-# The application is licensed under the ?? GNU General Public License V3.0 - decision on this? ?? 
+# Is is part of a program sponsered by the Education Incentive Funds of Utrecht University.
+# The lay-out for the shiny applications for this program is developed by Kimberley Lek.
+# The application is licensed under the ?? GNU General Public License V3.0 - decision on this? ??
 
 # Author Comment:
 # Do not delete any lines of code that are commented out; the app is still under development
@@ -12,72 +12,111 @@
 # File description:
 # This file contains the server for the application related to correlation.
 
-# Loading libraries 
+
+
+
+
+
+
+# Loading libraries
 library(shiny)
+library(stringr)
+library(markdown)
+library(pander)
+
+
+
+
+# function derived from the highlightHTMLcells() function of the highlightHTML package
+colortable <- function(htmltab, css, style="table-condensed table-bordered"){
+  tmp <- str_split(htmltab, "\n")[[1]]
+  CSSid <- gsub("\\{.+", "", css)
+  CSSid <- gsub("^[\\s+]|\\s+$", "", CSSid)
+  CSSidPaste <- gsub("#", "", CSSid)
+  CSSid2 <- paste(" ", CSSid, sep = "")
+  ids <- paste0("<td id='", CSSidPaste, "'")
+  for (i in 1:length(CSSid)) {
+    locations <- grep(CSSid[i], tmp)
+    tmp[locations] <- gsub("<td", ids[i], tmp[locations])
+    tmp[locations] <- gsub(CSSid2[i], "", tmp[locations],
+                           fixed = TRUE)
+  }
+  htmltab <- paste(tmp, collapse="\n")
+  Encoding(htmltab) <- "UTF-8"
+  list(
+    tags$style(type="text/css", paste(css, collapse="\n")),
+    tags$script(sprintf(
+      '$( "table" ).addClass( "table %s" );', style
+    )),
+    HTML(htmltab)
+  )
+}
+
+
 
 # server design
 server <- function(input, output) {
-  
+
 # set.seed(2)
 #data<-reactive({rnorm(input$samplesize, 7,3)})
 
-# The user needs to click on the action button labeled "Draw Sample" for the plot and descriptives to appear    
+# The user needs to click on the action button labeled "Draw Sample" for the plot and descriptives to appear
   data<-eventReactive(input$do_sample,
                       {rnorm(input$samplesize, 7,3)})
-# Draws boxplot in the first row of the app  
+# Draws boxplot in the first row of the app
     output$plot1 <- renderPlot({
 
-      y=data() 
-      
+      y=data()
+
       if(input$transform=="Add Outlier"){
-        
+
         y_n<-sort(y, decreasing=TRUE)
         y_m<-y_n[2:length(y_n)]
         y<-c(29,y_m) # The outlier will always be equal to 29
-             } 
+             }
       if(input$transform=="Add 5 points to each score"){
         y=data()+5
-              } 
+              }
 
        if(input$transform=="Multiply all scores by 2"){
       y=data()*2
-              } 
+              }
       if(input$transform=="Divide all scores by 2"){
         y=data()/2
-              } 
+              }
       if(input$transform=="Calculate z-scores"){
         y=(data()-mean(data()))/sd(data())
-              } 
+              }
     boxplot(y,xlab="x",ylab="", xlim =c(0,2) , ylim=c(-2,30), main = "Boxplot ", horizontal = TRUE)
 
-       abline(v=mean(y),col="blue") 
+       abline(v=mean(y),col="blue")
        abline(v=median(y), col="red")
 
   })
 
   # Table 1, table containing the mean, standard deviation, variance, minimum, first quartile, median, third quartile, and maximum
-   output$descriptives <- renderTable({
+   output$descriptives <- renderUI({
         y=data()
-        
+
         if(input$transform=="Add Outlier"){
 
           y_n<-sort(y, decreasing=TRUE)
           y_m<-y_n[2:length(y_n)]
           y<-c(29,y_m)
                 } else
-          
+
      if(input$transform=="Add 5 points to each score"){
         y=data()+5
-          } else 
+          } else
      if(input$transform=="Multiply all scores by 2"){
        y=2*data()
-     } else 
+     } else
        if(input$transform=="Divide all scores by 2"){
          y=data()/2
        } else
      if(input$transform=="Calculate z-scores"){
        y=(data()-mean(data()))/sd(data())
-     } 
+     }
 # All sample descriptives are rounded to 2 decimal points
      median<-round(median(y),2)
      mean<-round(mean(y),2)
@@ -87,15 +126,39 @@ server <- function(input, output) {
      Q3<-round(quantile(y)[4],2)
      Minimum<-round(min(y),2)
      Maximum<-round(max(y),2)
-     table.desc <- cbind(mean,Std.Deviation, Variance, Minimum, Q1, 
-                         median, Q3, Maximum) 
+     table.desc <- cbind(mean,Std.Deviation, Variance, Minimum, Q1,
+                         median, Q3, Maximum)
        table.desc.trans.1<-t(table.desc)
        names<-rbind("Mean","Standard Deviation","Variance",
                     "Minimum", "Q1", "Median", "Q3", "Maximum")
        table.desc.trans<-cbind(names,table.desc.trans.1)
        colnames(table.desc.trans)<-c(" ", " ")
-       print(table.desc.trans, colnames = TRUE)
-      
+
+
+       # This part of the code adds colors to some rows.
+       # define CSS tags
+       css <- c("#meancolor   {color: #0000FF;}",
+                "#mediancolor {color: #FF0000;}")
+       # example data frame
+       # add the tag inside the cells
+
+       table.desc.trans[1, ] <- paste(table.desc.trans[1, ], "#meancolor")
+       table.desc.trans[6, ] <- paste(table.desc.trans[6, ], "#mediancolor")
+
+       print(table.desc.trans)
+
+       # generate html table with pander package and markdown package
+       htmltab <- markdownToHTML(
+         text = pandoc.table.return(
+           table.desc.trans,
+           style = "rmarkdown",
+           split.tables = Inf,
+           row.names = FALSE
+         ),
+         fragment.only = TRUE
+       )
+       colortable(htmltab, css)
+
   })
 
 }
--- ui.R
+++ ui.R
@@ -1,8 +1,8 @@
 # Copyright statement:
 # This shiny apllication is developed by Duco Veen to be used for educational purposes.
-# Is is part of a program sponsered by the Education Incentive Funds of Utrecht University. 
-# The lay-out for the shiny applications for this program is developed by Kimberley Lek. 
-# The application is licensed under the ?? GNU General Public License V3.0 - decision on this? ?? 
+# Is is part of a program sponsered by the Education Incentive Funds of Utrecht University.
+# The lay-out for the shiny applications for this program is developed by Kimberley Lek.
+# The application is licensed under the ?? GNU General Public License V3.0 - decision on this? ??
 
 # Author Comment:
 # I have tried to code this according to the Google R Style Guide to improve readibility:
@@ -12,35 +12,35 @@
 # File description:
 # This file contains the user interface (UI) for the application related to correlation.
 
-# Loading library's 
+# Loading library's
 library(shiny)
 library(shinydashboard)
 
 # user interface design
 ui <- dashboardPage(skin = "black",
-                    dashboardHeader(title = "Descriptives and outliers",titleWidth = 350), 
+                    dashboardHeader(title = "Descriptives and outliers",titleWidth = 350),
                     dashboardSidebar(width = 350,
                                      sidebarMenu(#menuItem("", tabName = "home", icon = icon("home")),
                                        menuItem("Descriptive Statistics", tabName = "tab1"),
-                                       menuItem("Disclaimer", tabName = "Disclaimer"), 
-                                       HTML("<br><br><br><br><br><br><br><br><br><br><br><br><br><br>"), 
+                                       menuItem("Disclaimer", tabName = "Disclaimer"),
+                                       HTML("<br><br><br><br><br><br><br><br><br><br><br><br><br><br>"),
                                        img(src= "logo.png", align = "left"),
-                                       HTML("<br><br><br><br><br><br><br>"), 
+                                       HTML("<br><br><br><br><br><br><br>"),
                                        div("Shiny app by",
                                            a(href="https://www.uu.nl/staff/MMiocevic",
                                              target = "_blank",
                                              #"Milica Mio&#269;evi&#263;"),align="right", style = "font-size: 10pt"),
                                              "Milica Miocevic"),align="right", style = "font-size: 10pt"),
-                                       
+
                                        div("Base R code by",
                                            a(href="https://www.uu.nl/staff/mmiocevic",target="_blank",
                                              #"Milica Mio&#269;evi&#263;"),align="right", style = "font-size: 10pt"),
                                              "Milica Miocevic"),align="right", style = "font-size: 10pt"),
-                                       
+
                                        div("Base Layout by",
                                            a(href="https://www.uu.nl/medewerkers/KMLek/0",target="_blank",
                                              "Kimberley Lek"),align="right", style = "font-size: 10pt"),
-                                       
+
                                        div("Shiny source files:",
                                            a(href="https://github.com/EducationalShinyUU/Descriptives_and_outliers_shiny",
                                              target="_blank","GitHub"),align="right", style = "font-size: 10pt")
@@ -68,7 +68,7 @@ ui <- dashboardPage(skin = "black",
                                                 .skin-black .main-header .navbar {
                                                 background-color: #EAC626;
                                                 }
-                                                /* toggle button when hovered  */                    
+                                                /* toggle button when hovered  */
                                                 .skin-black .main-header .navbar .sidebar-toggle:hover{
                                                 background-color: #EAC626;
                                                 }
@@ -93,54 +93,54 @@ ui <- dashboardPage(skin = "black",
                       tabItems(
                         # now for the content of the different tabs.
                         # Disclaimer tab, university disclaimer.
-                        tabItem(tabName = "Disclaimer", 
-                                box(width = 12, 
-                                    h5("Terms of Usage Utrecht Unversity Shiny Server", 
-                                       br(), 
+                        tabItem(tabName = "Disclaimer",
+                                box(width = 12,
+                                    h5("Terms of Usage Utrecht Unversity Shiny Server",
+                                       br(),
                                        br(),
                                        tags$ul(
-                                         tags$li("Purpose of the service 'utrecht-university.shinyapps.io'  
-                                                 is to provide a digital place for trying out, evaluating 
-                                                 and/or comparing methods developed by researchers of Utrecht University 
-                                                 for the scientific community worldwide. 
-                                                 The app and its contents may not be preserved in such a way that it can 
-                                                 be cited or can be referenced to. "), 
-                                         tags$li("The web application is provided 'as is' and 'as available' and is without any warranty. 
-                                                 Your use of this web application is solely at your own risk."), 
-                                         tags$li("	You must ensure that you are lawfully entitled and have full authority to upload 
-                                                 data in the web application. The file data must not contain any data which can raise 
-                                                 issues relating to abuse, confidentiality, privacy,  data protection, licensing, and/or 
-                                                 intellectual property. You shall not upload data with any confidential or proprietary information 
+                                         tags$li("Purpose of the service 'utrecht-university.shinyapps.io'
+                                                 is to provide a digital place for trying out, evaluating
+                                                 and/or comparing methods developed by researchers of Utrecht University
+                                                 for the scientific community worldwide.
+                                                 The app and its contents may not be preserved in such a way that it can
+                                                 be cited or can be referenced to. "),
+                                         tags$li("The web application is provided 'as is' and 'as available' and is without any warranty.
+                                                 Your use of this web application is solely at your own risk."),
+                                         tags$li("	You must ensure that you are lawfully entitled and have full authority to upload
+                                                 data in the web application. The file data must not contain any data which can raise
+                                                 issues relating to abuse, confidentiality, privacy,  data protection, licensing, and/or
+                                                 intellectual property. You shall not upload data with any confidential or proprietary information
                                                  that you desire or are required to keep secret. "),
                                          tags$li("By using this app you agree to be bound by the above terms.")
                                          ) # end tags$ul
                                          )
                                        ) # end box
                                        ), # end tab Item (disclaimer tab.)
-                        
+
                         # Descriptives tab
-                        tabItem(tabName = "tab1", 
-                                
+                        tabItem(tabName = "tab1",
+
                                 title = "Descriptives and Outliers",
                                 plotOutput("plot1"),
                                 hr(),
-                                
-                                 fluidRow( 
+
+                                 fluidRow(
                                    column(6,
-                                       selectInput("samplesize", "Sample size:", 
-                                                   choices = c(10,30,100), 
+                                       selectInput("samplesize", "Sample size:",
+                                                   choices = c(10,30,100),
                                                    multiple = FALSE),
                                        radioButtons("transform", "Changes to the data:",
                                                     c("None", "Add Outlier", "Add 5 points to each score",
                                                       "Multiply all scores by 2", "Divide all scores by 2","Calculate z-scores"))),
-                                   
-                                       column(6, 
+
+                                       column(6,
                                               actionButton("do_sample", "Draw sample"), # find a way to put drop-down and button on same level
-                                              tableOutput("descriptives"))
-                                       
+                                              uiOutput("descriptives"))
+
                                    ) # end fluidRow
                                 ) # end tabItem (Descriptives tab)
-                        
+
                                        ) # end tabItems
                                     ) # end dashboardbody
                                 ) # end dashboardpage
