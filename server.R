0900d1d8fb9163b3e8d751a4026738453fdb710a
Kees Mulder <keestimmulder@gmail.com>
2018-09-18 15:15:20 +0200

    Layout of code

--- server.R
+++ server.R
@@ -54,110 +54,107 @@ colortable <- function(htmltab, css, style="table-condensed table-bordered"){
 # server design
 server <- function(input, output) {
 
-# set.seed(2)
-#data<-reactive({rnorm(input$samplesize, 7,3)})
+  # set.seed(2)
+  #data<-reactive({rnorm(input$samplesize, 7,3)})
 
-# The user needs to click on the action button labeled "Draw Sample" for the plot and descriptives to appear
+  # The user needs to click on the action button labeled "Draw Sample" for the plot and descriptives to appear
   data<-eventReactive(input$do_sample,
                       {abs(rnorm(input$samplesize, 7,3))})
-# Draws boxplot in the first row of the app
-    output$plot1 <- renderPlot({
+  # Draws boxplot in the first row of the app
+  output$plot1 <- renderPlot({
 
-      y=data()
+    y=data()
 
-      if(input$transform=="Add Outlier"){
+    if(input$transform=="Add Outlier"){
 
-        y_n<-sort(y, decreasing=TRUE)
-        y_m<-y_n[2:length(y_n)]
-        y<-c(29,y_m) # The outlier will always be equal to 29
-             }
-      if(input$transform=="Add 5 points to each score"){
-        y=data()+5
-              }
+      y_n<-sort(y, decreasing=TRUE)
+      y_m<-y_n[2:length(y_n)]
+      y<-c(29,y_m) # The outlier will always be equal to 29
+    }
+    if(input$transform=="Add 5 points to each score"){
+      y=data()+5
+    }
 
-       if(input$transform=="Multiply all scores by 2"){
+    if(input$transform=="Multiply all scores by 2"){
       y=data()*2
-              }
-      if(input$transform=="Divide all scores by 2"){
-        y=data()/2
-              }
-      if(input$transform=="Calculate z-scores"){
-        y=(data()-mean(data()))/sd(data())
-              }
+    }
+    if(input$transform=="Divide all scores by 2"){
+      y=data()/2
+    }
+    if(input$transform=="Calculate z-scores"){
+      y=(data()-mean(data()))/sd(data())
+    }
     boxplot(y,xlab="x",ylab="",
             boxwex =  1.7,
             xlim =c(0,2) , ylim=c(-2,30), main = "Boxplot ",
             horizontal = TRUE)
     rug(y)
-       abline(v=mean(y),col="blue")
-       abline(v=median(y), col="red")
+    abline(v=mean(y),col="blue")
+    abline(v=median(y), col="red")
 
   })
 
   # Table 1, table containing the mean, standard deviation, variance, minimum, first quartile, median, third quartile, and maximum
-   output$descriptives <- renderUI({
-        y=data()
+  output$descriptives <- renderUI({
+    y=data()
 
-        if(input$transform=="Add Outlier"){
+    if(input$transform=="Add Outlier"){
 
-          y_n<-sort(y, decreasing=TRUE)
-          y_m<-y_n[2:length(y_n)]
-          y<-c(29,y_m)
-                } else
+      y_n<-sort(y, decreasing=TRUE)
+      y_m<-y_n[2:length(y_n)]
+      y<-c(29,y_m)
+    } else if (input$transform=="Add 5 points to each score"){
+      y=data()+5
+    } else if (input$transform=="Multiply all scores by 2"){
+      y=2*data()
+    } else if (input$transform=="Divide all scores by 2"){
+      y=data()/2
+    } else if (input$transform=="Calculate z-scores"){
+      y=(data()-mean(data()))/sd(data())
+    }
+    # All sample descriptives are rounded to 2 decimal points
+    median<-round(median(y),2)
+    mean<-round(mean(y),2)
+    Std.Deviation<-round(sd(y),2)
+    Variance<-round(var(y),2)
+    Q1<-round(quantile(y)[2],2)
+    Q3<-round(quantile(y)[4],2)
+    Minimum<-round(min(y),2)
+    Maximum<-round(max(y),2)
+    table.desc <- cbind(mean,Std.Deviation, Variance, Minimum, Q1,
+                        median, Q3, Maximum)
 
-     if(input$transform=="Add 5 points to each score"){
-        y=data()+5
-          } else
-     if(input$transform=="Multiply all scores by 2"){
-       y=2*data()
-     } else
-       if(input$transform=="Divide all scores by 2"){
-         y=data()/2
-       } else
-     if(input$transform=="Calculate z-scores"){
-       y=(data()-mean(data()))/sd(data())
-     }
-# All sample descriptives are rounded to 2 decimal points
-     median<-round(median(y),2)
-     mean<-round(mean(y),2)
-     Std.Deviation<-round(sd(y),2)
-     Variance<-round(var(y),2)
-     Q1<-round(quantile(y)[2],2)
-     Q3<-round(quantile(y)[4],2)
-     Minimum<-round(min(y),2)
-     Maximum<-round(max(y),2)
-     table.desc <- cbind(mean,Std.Deviation, Variance, Minimum, Q1,
-                         median, Q3, Maximum)
-       table.desc.trans.1<-t(table.desc)
-       names<-rbind("Mean","Standard Deviation","Variance",
-                    "Minimum", "Q1", "Median", "Q3", "Maximum")
-       table.desc.trans<-cbind(names,table.desc.trans.1)
-       colnames(table.desc.trans)<-c(" ", " ")
+    table.desc.trans.1<-t(table.desc)
+
+    names<-rbind("Mean","Standard Deviation","Variance",
+                 "Minimum", "Q1", "Median", "Q3", "Maximum")
+    table.desc.trans<-cbind(names,table.desc.trans.1)
+    colnames(table.desc.trans)<-c(" ", " ")
 
 
-       # This part of the code adds colors to some rows.
-       # define CSS tags
-       css <- c("#meancolor   {color: #0000FF;}",
-                "#mediancolor {color: #FF0000;}")
-       # example data frame
-       # add the tag inside the cells
+    # This part of the code adds colors to some rows.
+    # define CSS tags
+    css <- c("#meancolor   {color: #0000FF;}",
+             "#mediancolor {color: #FF0000;}")
+    # example data frame
+    # add the tag inside the cells
 
-       table.desc.trans[1, ] <- paste(table.desc.trans[1, ], "#meancolor")
-       table.desc.trans[6, ] <- paste(table.desc.trans[6, ], "#mediancolor")
+    table.desc.trans[1, ] <- paste(table.desc.trans[1, ], "#meancolor")
+    table.desc.trans[6, ] <- paste(table.desc.trans[6, ], "#mediancolor")
 
-       print(table.desc.trans)
+    print(table.desc.trans)
 
-       # generate html table with pander package and markdown package
-       htmltab <- markdownToHTML(
-         text = pandoc.table.return(
-           table.desc.trans,
-           style = "rmarkdown",
-           split.tables = Inf,
-           row.names = FALSE
-         ),
-         fragment.only = TRUE
-       )
-       colortable(htmltab, css)
+    # generate html table with pander package and markdown package
+    htmltab <- markdownToHTML(
+      text = pandoc.table.return(
+        table.desc.trans,
+        style = "rmarkdown",
+        split.tables = Inf,
+        row.names = FALSE
+      ),
+      fragment.only = TRUE
+    )
+    colortable(htmltab, css)
 
   })
 
